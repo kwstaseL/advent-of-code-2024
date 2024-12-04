2446324fb44238881823f3d3fc0439cef4956f65
kwstaseL <kostasel19@gmail.com>
2024-12-04 21:27:34 +0200

    Add problem 3 solution

---
+++ problem-3/sol.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"aoc2024/aoc_input"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+/*
+	Alternatively, do an exhaustive search for all possible multiplications and sum them up.
+*/
+func sumMultiplications(input string) int {
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	matches := re.FindAllStringSubmatch(input, -1)
+
+	sum := 0
+
+	for _, match := range matches {
+		if len(match) == 3 {
+			x, _ := strconv.Atoi(match[1])
+			y, _ := strconv.Atoi(match[2])
+			sum += x * y
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	input, _ := aoc_input.GetAocInput(3, 2024)
+	result := sumMultiplications(input)
+	fmt.Println("Total sum of valid multiplications:", result)
+}
