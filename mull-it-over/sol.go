0c7f32499f2823c269210b2321e944b0f47a37d8
kwstaseL <kostasel19@gmail.com>
2024-12-10 20:58:51 +0200

    Add mull-it-over part 2 sol

--- mull-it-over/sol.go
+++ mull-it-over/sol.go
@@ -5,17 +5,15 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-/*
-	Alternatively, do an exhaustive search for all possible multiplications and sum them up.
-*/
-func sumMultiplications(input string) int {
+// Part 1: Sum all valid mul(X,Y) operations
+func part1(input string) int {
 	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 	matches := re.FindAllStringSubmatch(input, -1)
 
 	sum := 0
-
 	for _, match := range matches {
 		if len(match) == 3 {
 			x, _ := strconv.Atoi(match[1])
@@ -27,8 +25,42 @@ func sumMultiplications(input string) int {
 	return sum
 }
 
+// Part 2: Sum only the enabled mul(X,Y) operations based on do() and don't()
+func part2(input string) int {
+	re := regexp.MustCompile(`do\(\)|don't\(\)|mul\((\d+),(\d+)\)`)
+	matches := re.FindAllStringSubmatch(input, -1)
+
+	sum := 0
+	enabled := true
+
+	for _, match := range matches {
+		instruction := strings.TrimSpace(match[0])
+
+		if instruction == "do()" {
+			enabled = true
+		} else if instruction == "don't()" {
+				enabled = false
+		} else {
+			if enabled {
+				x, _ := strconv.Atoi(match[1])
+				y, _ := strconv.Atoi(match[2])
+				sum += x * y
+			}
+		}
+	}
+
+	return sum
+}
+
 func main() {
-	input, _ := aoc_input.GetAocInput(3, 2024)
-	result := sumMultiplications(input)
-	fmt.Println("Total sum of valid multiplications:", result)
+	input, err := aoc_input.GetAocInput(3, 2024)
+	if err != nil {
+		fmt.Printf("Error fetching input: %v\n", err)
+		return
+	}
+	part1Result := part1(input)
+	fmt.Println("Part 1 - Total sum of valid multiplications:", part1Result)
+
+	part2Result := part2(input)
+	fmt.Println("Part 2 - Total sum of valid enabled multiplications:", part2Result)
 }
